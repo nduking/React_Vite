50aca2cdcb5224cfa5fc4d2cc16bbf09b7d6b3fc
nduking <114291880+nduking@users.noreply.github.com>
2023-06-27 16:45:54 +0100

    fixed issue

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import MainComponent from './componets/hcomp/MainComponent'
-import Footer from './componets/hcomp/Footer'
+
+import MainContent from './componets/hcomp/MainComponent';
+import Navbar from './componets/hcomp/Navbar';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+   return (
     <div className="font-spaceGrotesk px-24 py-10">
       <Navbar />
       <MainContent />
